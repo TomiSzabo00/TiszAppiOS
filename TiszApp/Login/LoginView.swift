6c7cec73829fcc9cde8512ddeab278c6427f1494
TomiSzabo00 <tamas.szabo0102@gmail.com>
2022-04-03 16:50:15 +0200

    minor adjustments

--- TiszApp/Login/LoginView.swift
+++ TiszApp/Login/LoginView.swift
@@ -17,63 +17,68 @@ struct LoginView: View {
     var body: some View {
         ZStack {
             Color.offWhite.ignoresSafeArea()
-        VStack{
-            Spacer()
-            Image("icon")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 180, height: 180)
-                .padding()
-            
-            VStack{
+            //ScrollView{
                 VStack{
-                    SimpleTextFieldWithIcon(textField:
-                    TextField("Felhasználónév", text: $vm.details.userName), imageName: "person.fill")
-                    .padding(.bottom, 5)
-                    SimpleSecureTextField(secureFied: SecureField("Jelszó", text: $vm.details.password), imageName: "lock.fill")
-                }
-                .padding()
-                
-                Button(action: {
-                    //login
-                    vm.login()
-                }, label: { Text("Bejelentkezés").bold()})
-                .buttonStyle(SimpleButtonStyle())
-                .padding(.bottom, 30)
-                
-                HStack{
-                    Text("Nincs még fiókod?")
-                    Button(action: {
-                        showRegistration.toggle()
-                    }, label: {Text("Regisztrálj!").bold()})
-                    .sheet(isPresented: $showRegistration, content: { RegisterView()})
+                    Spacer()
+                    
+                    Image("icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 180, height: 180)
+                        .padding()
+                    
+                    VStack{
+                        VStack{
+                            SimpleTextFieldWithIcon(textField:
+                                                        TextField("Felhasználónév", text: $vm.details.userName), imageName: "person.fill")
+                            .padding(.bottom, 5)
+                            SimpleSecureTextField(secureFied: SecureField("Jelszó", text: $vm.details.password), imageName: "lock.fill")
+                        }
+                        .padding()
+                        
+                        Button(action: {
+                            //login
+                            vm.login()
+                        }, label: { Text("Bejelentkezés").bold()})
+                        .buttonStyle(SimpleButtonStyle())
+                        .padding(.bottom, 30)
+                        
+                        HStack{
+                            Text("Nincs még fiókod?")
+                            Button(action: {
+                                showRegistration.toggle()
+                            }, label: {Text("Regisztrálj!").bold()})
+                            .sheet(isPresented: $showRegistration, content: { RegisterView()})
+                            Spacer()
+                        }
+                    }
                     Spacer()
                 }
+                .alert(isPresented: $vm.hasError, content: {
+                    if case .failed(let error) = vm.state {
+                        return Alert(title: Text("Hiba"),
+                                     message: Text(customError(error:error)))
+                    } else {
+                        return Alert(title: Text("Hiba"),
+                                     message: Text("Valami hiba történt. Próbáld újra."))
+                    }
+                    
+                })
+                .onTapGesture {
+                    endTextEditing()
+                }
+                .padding(.leading)
+                .padding(.trailing)
             }
-            Spacer()
-            Spacer()
+            .navigationBarHidden(true)
         }
-        .alert(isPresented: $vm.hasError, content: {
-            if case .failed(let error) = vm.state {
-                return Alert(title: Text("Hiba"),
-                             message: Text(customError(error:error)))
-            } else {
-                return Alert(title: Text("Hiba"),
-                message: Text("Valami hiba történt. Próbáld újra."))
-            }
-            
-        })
-        .onTapGesture {
-            endTextEditing()
-        }
-        .padding(.leading)
-        .padding(.trailing)
-    }
-    }
+   // }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        NavigationView{
+            LoginView()
+        }
     }
 }
--- TiszApp/Registration/RegisterView.swift
+++ TiszApp/Registration/RegisterView.swift
@@ -28,27 +28,27 @@ func customError(error: Error) -> String {
     var myError: String = "No match found for error type..."
     if let errCode = AuthErrorCode(rawValue: error._code) {
         switch errCode {
-            case .emailAlreadyInUse:
-                myError = "Ez a felhasználónév már foglalt."
-            case .invalidEmail:
-                myError = "Hibás formátum: A felhasználónév 1 szóból állhat csak, speciális karakterek nélkül."
-            case .userNotFound:
-                myError = "Ilyen felhasználónév nincs regisztrálva."
-            case .networkError:
-                myError = "Hálózat nem elérhető. Kapcsold be a mobilnetet, vagy próbáld meg később."
-            case .weakPassword:
-                myError = "A jelszó túl rövid. Legalább 6 karakter hosszúnak kell lennie."
-            case .wrongPassword:
-                myError = "Hibás jelszó."
-            default:
-                myError = "Ismeretlen a hiba oka."
-            }
+        case .emailAlreadyInUse:
+            myError = "Ez a felhasználónév már foglalt."
+        case .invalidEmail:
+            myError = "Hibás formátum: A felhasználónév 1 szóból állhat csak, speciális karakterek nélkül."
+        case .userNotFound:
+            myError = "Ilyen felhasználónév nincs regisztrálva."
+        case .networkError:
+            myError = "Hálózat nem elérhető. Kapcsold be a mobilnetet, vagy próbáld meg később."
+        case .weakPassword:
+            myError = "A jelszó túl rövid. Legalább 6 karakter hosszúnak kell lennie."
+        case .wrongPassword:
+            myError = "Hibás jelszó."
+        default:
+            myError = "Ismeretlen a hiba oka."
         }
+    }
     return myError
 }
 
 struct RegisterView: View {
-
+    
     @Environment(\.presentationMode) var presentationMode
     
     @StateObject private var vm = RegistrationViewModelImpl(service: RegistrationServiceImpl())
@@ -72,112 +72,112 @@ struct RegisterView: View {
         default:
             NavigationView{
                 ScrollView{
-                VStack{
-                    Spacer()
-                    VStack(spacing: 20){
-                        PredictingTextField(predictableValues: $vm.service.allUserNames, predictedValues: self.$predictedValue, textFieldInput: $vm.userDetails.fullName, textFieldTitle: prefix)
-                        ZStack {
-                            TextField("Egyedi azonosító", text: $vm.userDetails.id)
+                    VStack{
+                        Spacer()
+                        VStack(spacing: 20){
+                            PredictingTextField(predictableValues: $vm.service.allUserNames, predictedValues: self.$predictedValue, textFieldInput: $vm.userDetails.fullName, textFieldTitle: prefix)
+                            ZStack {
+                                TextField("Egyedi azonosító", text: $vm.userDetails.id)
+                                    .padding()
+                                    .background(Color(.secondarySystemBackground))
+                                    .cornerRadius(10)
+                                    .disableAutocorrection(true)
+                                    .keyboardType(.numberPad)
+                                
+                                if self.predictedValue.count > 0 {
+                                    ScrollView {
+                                        ForEach(self.predictedValue, id: \.self) { value in
+                                            VStack {
+                                                Button(action: {
+                                                    endTextEditing()
+                                                    vm.userDetails.fullName = value
+                                                }, label: {
+                                                    Text(value)
+                                                })
+                                                .foregroundColor(Color.primary)
+                                                .padding(10)
+                                                .frame(maxWidth: .infinity)
+                                                
+                                                Rectangle().fill(Color.primary).frame(width: 250, height: 1, alignment: .center)
+                                            }
+                                        }
+                                        
+                                    }
+                                    .background(.ultraThinMaterial)
+                                    .cornerRadius(20)
+                                    .frame(height: 200, alignment: .center)
+                                }
+                                
+                            } //ZStack
+                            
+                        }
+                        .padding()
+                        
+                        
+                        VStack{
+                            TextField("Felhasználónév", text: $vm.userDetails.userName)
                                 .padding()
                                 .background(Color(.secondarySystemBackground))
                                 .cornerRadius(10)
                                 .disableAutocorrection(true)
-                                .keyboardType(.numberPad)
+                                .autocapitalization(.none)
+                            SecureField("Jelszó", text: $vm.userDetails.password)
+                                .padding()
+                                .background(Color(.secondarySystemBackground))
+                                .cornerRadius(10)
+                            SecureField("Jelszó megint", text: $vm.userDetails.password2)
+                                .padding()
+                                .background(Color(.secondarySystemBackground))
+                                .cornerRadius(10)
                             
-                            if self.predictedValue.count > 0 {
-                                ScrollView {
-                                    ForEach(self.predictedValue, id: \.self) { value in
-                                        VStack {
-                                            Button(action: {
-                                                endTextEditing()
-                                                vm.userDetails.fullName = value
-                                            }, label: {
-                                                Text(value)
-                                            })
-                                            .foregroundColor(Color.primary)
-                                            .padding(10)
-                                            .frame(maxWidth: .infinity)
-                                            
-                                            Rectangle().fill(Color.primary).frame(width: 250, height: 1, alignment: .center)
-                                        }
-                                    }
-                                        
-                                }
-                                .background(.ultraThinMaterial)
-                                .cornerRadius(20)
-                                .frame(height: 200, alignment: .center)
+                            
+                            HStack{
+                                Spacer()
+                                Button(action: {
+                                    
+                                    //register
+                                    vm.register()
+                                    
+                                }, label: { Text("Regisztrálok").padding()})
+                                .frame(width: 200, height: 50)
+                                .background(Color.blue)
+                                .foregroundColor(Color.white)
+                                .cornerRadius(10)
+                                .padding(.top)
                             }
-                            
-                        } //ZStack
-                        
-                    }
-                    .padding()
-                    
-                    
-                    VStack{
-                        TextField("Felhasználónév", text: $vm.userDetails.userName)
-                            .padding()
-                            .background(Color(.secondarySystemBackground))
-                            .cornerRadius(10)
-                            .disableAutocorrection(true)
-                            .autocapitalization(.none)
-                        SecureField("Jelszó", text: $vm.userDetails.password)
-                            .padding()
-                            .background(Color(.secondarySystemBackground))
-                            .cornerRadius(10)
-                        SecureField("Jelszó megint", text: $vm.userDetails.password2)
-                            .padding()
-                            .background(Color(.secondarySystemBackground))
-                            .cornerRadius(10)
-                        
-                        
-                        HStack{
-                            Spacer()
-                            Button(action: {
-                                
-                                //register
-                                vm.register()
-                                
-                            }, label: { Text("Regisztrálok").padding()})
-                            .frame(width: 200, height: 50)
-                            .background(Color.blue)
-                            .foregroundColor(Color.white)
-                            .cornerRadius(10)
-                            .padding(.top)
                         }
+                        .padding()
+                        Spacer()
                     }
+                    .alert(isPresented: $vm.hasError, content: {
+                        if vm.errorType != .na{
+                            return customAlert(errTpye: vm.errorType)
+                        } else if case .failed(let error) = vm.state {
+                            return Alert(title: Text("Hiba"),
+                                         message: Text(customError(error:error)))
+                        } else {
+                            return Alert(title: Text("Hiba"),
+                                         message: Text("Valami hiba történt. Próbáld újra."))
+                        }
+                    })
+                    .onTapGesture {
+                        endTextEditing()
+                    }
+                    .navigationBarTitle("Regisztráció", displayMode: .automatic)
+                    .toolbar{ToolbarItem(placement: .principal){
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }, label: {HStack{
+                            Image(systemName: "arrow.down")}
+                            Text("Vissza")
+                        }
+                        )}}
                     .padding()
-                    Spacer()
                 }
-                .alert(isPresented: $vm.hasError, content: {
-                    if vm.errorType != .na{
-                        return customAlert(errTpye: vm.errorType)
-                    } else if case .failed(let error) = vm.state {
-                        return Alert(title: Text("Hiba"),
-                                     message: Text(customError(error:error)))
-                    } else {
-                        return Alert(title: Text("Hiba"),
-                        message: Text("Valami hiba történt. Próbáld újra."))
-                    }
-                })
-                .onTapGesture {
-                    endTextEditing()
-                }
-                .navigationBarTitle("Regisztráció", displayMode: .automatic)
-                .toolbar{ToolbarItem(placement: .principal){
-                    Button(action: {
-                        presentationMode.wrappedValue.dismiss()
-                    }, label: {HStack{
-                        Image(systemName: "arrow.down")}
-                        Text("Vissza")
-                    }
-                    )}}
-                .padding()
             }
         }
-        }
         
-  }
+    }
 }
 
 struct RegisterView_Previews: PreviewProvider {
